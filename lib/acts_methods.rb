b8e721547bdcd424424eabd9a7a1754141cf765d
tomw <tomw@cd36c3d2-5312-0410-b821-e38f4cdfd9be>
2008-07-07 14:56:57 +0000

    Changed solr type_field config to be string rather than text, so that type fields are neither tokenized nor (more dangerously) stemmed (part of changeset #4634 in the original solr branch)
    git-svn-id: svn://svn.jgpaspire.co.uk/aspire/trunk/vendor/plugins/acts_as_solr@6384 cd36c3d2-5312-0410-b821-e38f4cdfd9be

--- lib/acts_methods.rb
+++ lib/acts_methods.rb
@@ -115,8 +115,9 @@ module ActsAsSolr #:nodoc:
         :boost => nil,
         :if => "true"
       }  
+      
       self.solr_configuration = {
-        :type_field => "type_t",
+        :type_field => "type_s",
         :primary_key_field => "pk_i",
         :default_boost => 1.0
       }
